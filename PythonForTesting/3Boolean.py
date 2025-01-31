ddf35c08d6aee2cfe2d68b88cb4bfbff6d6ea940
23Atul <atulraman23@gmail.com>
2025-01-31 11:48:10 +0530

    boolean

---
+++ PythonForTesting/3Boolean.py
@@ -0,0 +1,17 @@
+x = 0
+y = 20
+
+print(x > y)  # False
+
+
+print( bool(x)) # False
+print( bool(y)) # True
+
+print( bool("")) # False
+print(bool("A")) # True
+
+print( bool([])) # False
+print(bool(["B"])) # True
+
+print(bool(-1)) # True
+
