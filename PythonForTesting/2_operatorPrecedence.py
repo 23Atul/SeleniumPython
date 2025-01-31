269a4877883883de509db9bc45caf0f06c3864f5
23Atul <atulraman23@gmail.com>
2025-01-31 12:28:53 +0530

    strings

--- PythonForTesting/2_operatorPrecedence.py
+++ PythonForTesting/2_operatorPrecedence.py
@@ -9,4 +9,24 @@ print(5 - (8 + 2)) # -5 --->> 5 - (10) = -5  () has more precedence
 
 #---------------------------------------------------------------------------------------------
 
+# Highest to Least,   Left to Right  -->
 
+# 1.    [expression], {key:value} { expression}
+
+# 2.    (expression)
+
+# 3.    **
+
+# 4.    *, /, //, %
+
+# 5.    +, -
+
+# 6.    not
+
+# 7.    and 
+
+# 8.    or
+
+# 9.    if-else
+
+# 10.   =
---
+++ PythonForTesting/4_strings.py
@@ -0,0 +1,47 @@
+
+#-------------------- S T R I N G S ----------------------------
+
+x = "this is a string"
+
+y = 'this is string in single quotes'
+
+# multi-line String
+m = '''geek 
+           for 
+               geeks'''
+
+
+
+print(x)
+print(y)
+print(m)
+
+
+# * strings are indexed from 0 
+
+print(x[0]) # t --> prints first character
+
+#---------------------------------------------------
+
+# x = "this is "ATUL" pen"   # error
+# x = 'this is 'ATUL' pen'   # error
+
+# x = "this is 'Atul' pen"     # correct
+# x = 'this is "ATUL" pen'     # correct
+
+
+
+#---------------------------------------------------------
+
+# membership operator in string
+
+x = " this is ann example of string"
+
+print( "of" in x)  # True
+print("ATUL" in x) # False
+
+#-------------------------------------------------------------
+
+
+
+
