02efb7a11577a81a90c2041528e9f1a9ce6f090e
23Atul <atulraman23@gmail.com>
2025-02-11 14:25:57 +0530

    functions

---
+++ PythonForTesting/22_functions.py
@@ -0,0 +1,45 @@
+
+# ------------------- F U N C T I O N S ------------------------
+
+# functions are the reusable piece of code
+
+# traditional approach
+
+x = 10
+y = 23
+z = x + y
+print(z) # 33
+
+
+a = 99
+b = 101
+c = a + b
+print(c) # 200
+
+
+
+# using functions
+
+def addition1():
+    print(23+10)
+
+addition1() # 33
+
+#---------------------------------
+
+x=99
+y=101
+def addition2():
+    print(x+y)
+
+addition2()  # 200
+
+# ------------------------------------
+
+
+def addition3(x,y):
+    print(x+y)
+
+addition3(23,33) # 56
+addition3(99,101) # 200
+
