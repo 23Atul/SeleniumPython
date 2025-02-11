76c68dd247229255c46eea137712baa8bd649941
23Atul <atulraman23@gmail.com>
2025-02-11 07:37:19 +0530

    if-else

---
+++ PythonForTesting/16_if_else.py
@@ -0,0 +1,26 @@
+
+# --------------------------- if - else ------------------------------------
+
+score = 87
+
+if score > 90:
+    print("you are a topper")
+else:
+    print("you need to work harder")
+
+
+
+
+
+# ----------------------------if - elif - else (else - if) --------------------------------
+
+score = 33
+
+if score > 90:
+    print("you are a topper")
+elif score > 80 and score <= 90:
+    print("work little bit harder to become a topper")
+elif score > 60 and score <= 80:
+    print("you can improve")
+else:
+    print("poor performance")
