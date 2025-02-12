d819d1dce82d26f50d1f15a3d7718d52eb000d60
23Atul <atulraman23@gmail.com>
2025-02-13 02:17:13 +0530

    modules

--- PythonForTesting/34_modules.py
+++ PythonForTesting/34_modules.py
@@ -1,8 +1,9 @@
 
 # modules: piece of code which can be used anywhere in any file or folder ny simply just importing that module.
 
-# we want to use the functions defined in 34.1_myModule.py file but we dont want to rewrite the whole code here as well, so we can simply import the module here.
+# we want to use the functions defined in myfirstModule.py file but we dont want to rewrite the whole code here as well, so we can simply import the module here.
 
+from math import factorial
 
 from myModule import myfirstModule
 
@@ -14,7 +15,6 @@ myfirstModule.fun2()  # function2
 
 # -------------- built-in module --------------------
 
-from math import factorial
 
 print(factorial(20))  # 2432902008176640000
 print(factorial(4))   # 24
