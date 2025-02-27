68e5d2dd601fdafdb96eaee8feb80752f52d1f47
23Atul <atulraman23@gmail.com>
2025-02-27 23:46:20 +0530

    simgle select drop down

---
+++ 1_Selenium/19.1_DropDown_singleSelect.py
@@ -0,0 +1,37 @@
+
+# here we will look how we can select a value from dropdown that allows only one value to get selected.
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By  # to use By
+import time
+from selenium.webdriver.support.select import Select
+
+
+class DemoDropdown:
+
+    def Dropdown(self):
+
+        # we can select and unselect the radio button by .click() method
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        driver.get("https://testautomationcentral.com/demo/dropdown.html")
+        
+        dropdown = driver.find_element(By.XPATH, "//div[@id='simple-dropdown']//select[@class='form-select block w-full mt-1']")
+        dd = Select(dropdown)  # checks if the dropdown has a select and options in or not, if no it returns an exception error.
+        
+        # 1. select_by_index()
+        dd.select_by_index(1)  # selects the 1st option in the select dropdown.
+        time.sleep(2)
+
+        # 2. select_by_value(value)
+        dd.select_by_value("option2")  #<option value="option2">Option 2</option>
+        time.sleep(2)
+
+        # 3. select_by_visible_text("text")
+        dd.select_by_visible_text("Option 3")  # selects the provided value from the dropdown
+        time.sleep(2)
+
+
+findbyXpath = DemoDropdown()
+findbyXpath.Dropdown()
