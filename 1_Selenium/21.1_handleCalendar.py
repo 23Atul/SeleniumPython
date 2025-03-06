a1c924e57e884b6cf97baed36e83b8d93ca5b59b
23Atul <atulraman23@gmail.com>
2025-03-06 21:26:29 +0530

    handle Calendar

--- 1_Selenium/21.1_handleCalendar.py
+++ 1_Selenium/21.1_handleCalendar.py
@@ -15,6 +15,8 @@ class DemohandleCalendar:
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         driver.get("https://www.yatra.com/")
 
+# for departure date
+
         d_date = driver.find_element(By.XPATH, "//div[@class='css-w7k25o']")
         d_date.click()
         all_dates = driver.find_elements(By.XPATH, "//div[@class= 'react-datepicker__month-container']//div[@class='react-datepicker__month']//div[@class!='react-datepicker__day--disabled ']")
@@ -26,6 +28,24 @@ class DemohandleCalendar:
     
                 date.click()
                 time.sleep(2)
+                break
+
+    
+# for returning date
+        r_date = driver.find_element(By.XPATH, "//span[contains(@class,'css-xf2jy5')]")
+        r_date.click()
+        all_dates = driver.find_elements(
+            By.XPATH, "//div[@class= 'react-datepicker__month-container']//div[@class='react-datepicker__month']//div[@class!='react-datepicker__day--disabled ']")
+        time.sleep(2)
+
+        for date in all_dates:
+            # print(date.get_attribute("aria-label"))
+            if date.get_attribute("aria-label") == "Choose Sunday, April 20th, 2025":
+
+                date.click()
+                time.sleep(2)
+                break
+
 
 
 # //div[@class = 'react-datepicker__month-container']//div[@class = "react-datepicker__month"]//div[@class != "react-datepicker__day--disabled "]
