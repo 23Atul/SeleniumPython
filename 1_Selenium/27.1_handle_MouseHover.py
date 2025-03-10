10a0a2daac9dfaced983ad56ad9e18dfa3c890c7
23Atul <atulraman23@gmail.com>
2025-03-10 23:30:59 +0530

    mouse hover

--- 1_Selenium/27.1_handle_MouseHover.py
+++ 1_Selenium/27.1_handle_MouseHover.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By  # to use By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 
 
 class demo_MouseHover:
@@ -16,12 +17,49 @@ class demo_MouseHover:
         driver.get("https://unstop.com/")
         driver.maximize_window()
         
-
+#
         # trying to aceess the element without mouse hover,
         # if we try to access those elements then it will throw an error as they are not accessable without hover.
+
+        # locating the element on which we have to hover
+
+        more_btn = driver.find_element(By.XPATH, "//span[@aria-label='Browse More Categories']")
+
+        act_chain = ActionChains(driver) # creating obj of class ActionChains
+        time.sleep(2)
+
+        # action chain helps us to chain many actions one after another like, hover then double click the click.
+        # so after every action is chained they are waiting in a queue to make them happen we write .perform()
+        act_chain.move_to_element(more_btn).perform() # driver moves/hover over to the more button
+        # the above hover action was performed after i added perform().
+
+        time.sleep(3)
+
+        # clicking on element which was not visible directly but visible when hovered.
+        driver.find_element(By.XPATH, "//strong[@aria-label='Conferences']").click()
 
         time.sleep(3)
 
 
 findXPATH = demo_MouseHover()
 findXPATH.MouseHover()
+
+
+# What Does .perform() Do?
+# The .perform() method executes all the actions that were queued up using ActionChains. Without .perform(), Selenium only stores the actions but doesn't actually run them.
+
+# In your case, when you wrote:
+
+# python
+# Copy
+# Edit
+# act_chain.move_to_element(more_btn) 
+# Selenium prepared the hover action but didn't execute it.
+
+# When you added:
+
+# python
+# Copy
+# Edit
+# act_chain.move_to_element(more_btn).perform()
+# Selenium immediately executed the hover action, causing the dropdown menu to appear.
