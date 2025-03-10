be5806e4bac95bc4f0952388791a0d6951c99868
23Atul <atulraman23@gmail.com>
2025-03-10 16:21:48 +0530

    alert handling

--- 1_Selenium/26.1_handleAlerts.py
+++ 1_Selenium/26.1_handleAlerts.py
@@ -15,14 +15,40 @@ class demo_handleAlerts:
 
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         driver.get("https://www.w3schools.com/js/tryit.asp?filename=tryjs_prompt")
+        driver.maximize_window()
 
         # in w3 school this alert part is in different frame so switching the frame.
         # switch with name
         driver.switch_to.frame("iframeResult")
         time.sleep(2)
+        
+    # we going to click the button to launch the popup box after every action   
+    # till this step we are clicking on the button and popup appears, now how to handle that popup.
+
+        # accept the alert ie. click on OK
         driver.find_element(By.XPATH, "//button[normalize-space()='Try it']").click()
         time.sleep(3)
+        driver.switch_to.alert.accept() 
+        time.sleep(2)
         
+        # dismisses the alert ie. click on CANCEL
+        driver.find_element(By.XPATH, "//button[normalize-space()='Try it']").click()
+        time.sleep(3)
+        driver.switch_to.alert.dismiss()
+        time.sleep(2)
+
+        # use to send required user input in Prompt box.
+        driver.find_element(By.XPATH, "//button[normalize-space()='Try it']").click()
+        time.sleep(3)
+        driver.switch_to.alert.send_keys("Atul")
+        driver.switch_to.alert.accept()
+        time.sleep(4)
+
+        driver.find_element(By.XPATH, "//button[normalize-space()='Try it']").click()
+        time.sleep(2)
+        print(driver.switch_to.alert.text) # prints the text written in popup box
+        # Please enter your name:
+
 
 
 
